653d57f11402372a7f04b8b729a39d74d60d4751
Philip Cronje <syke107@gmail.com>
2014-02-24 17:58:37 +0200

    Added settings activity sans interaction logic
     * Registered activity as Muzei settings activity for art source.
     * Added themes/styles used in settings activity.
     * Added layout for settings activity.
       * Layout uses tools:textColor to style radio buttons in Android Studio
         preview to work around inability of previews to correctly use the color
         state list.

---
+++ src/main/java/net/za/slyfox/muzei/awprs/AndroidWallpapersSettingsActivity.java
@@ -0,0 +1,28 @@
+/* ********************************************************************************************** */
+/*
+ * Copyright (C) 2014, Philip Cronje
+ * All rights reserved.
+ *
+ * Distributed under the terms of the BSD 2-Clause License. See LICENSE in the source distribution
+ * for the full terms of the license.
+ */
+/* ********************************************************************************************** */
+package net.za.slyfox.muzei.awprs;
+
+/* ********************************************************************************************** */
+import android.app.Activity;
+import android.os.Bundle;
+
+/* ********************************************************************************************** */
+public class AndroidWallpapersSettingsActivity extends Activity
+{
+	@Override protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
+
+	} // onCreate
+
+} // AndroidWallpapersSettingsActivity
+
+/* ********************************************************************************************** */
